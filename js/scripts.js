8c399a485847ce3cd9e3c90ed7b9367eadd9449a
WaruiAlfred <alfredwarui17@gmail.com>
2023-12-20 17:57:09 +0300

    add js mouseover eventlisteners

---
+++ js/scripts.js
@@ -0,0 +1,66 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+  const work1 = document.getElementById("work1");
+  const work2 = document.getElementById("work2");
+  const work3 = document.getElementById("work3");
+  const work4 = document.getElementById("work4");
+  const work5 = document.getElementById("work5");
+  const work6 = document.getElementById("work6");
+  const work7 = document.getElementById("work7");
+  const work8 = document.getElementById("work8");
+
+  work1.addEventListener("mouseover", function () {
+    document.getElementById("overtext1").style.display = "block";
+  });
+  work1.addEventListener("mouseout", function () {
+    document.getElementById("overtext1").style.display = "none";
+  });
+
+  work2.addEventListener("mouseover", function () {
+    document.getElementById("overtext2").style.display = "block";
+  });
+  work2.addEventListener("mouseout", function () {
+    document.getElementById("overtext2").style.display = "none";
+  });
+
+  work3.addEventListener("mouseover", function () {
+    document.getElementById("overtext3").style.display = "block";
+  });
+  work3.addEventListener("mouseout", function () {
+    document.getElementById("overtext3").style.display = "none";
+  });
+
+  work4.addEventListener("mouseover", function () {
+    document.getElementById("overtext4").style.display = "block";
+  });
+  work4.addEventListener("mouseout", function () {
+    document.getElementById("overtext4").style.display = "none";
+  });
+
+  work5.addEventListener("mouseover", function () {
+    document.getElementById("overtext5").style.display = "block";
+  });
+  work5.addEventListener("mouseout", function () {
+    document.getElementById("overtext5").style.display = "none";
+  });
+
+  work6.addEventListener("mouseover", function () {
+    document.getElementById("overtext6").style.display = "block";
+  });
+  work6.addEventListener("mouseout", function () {
+    document.getElementById("overtext6").style.display = "none";
+  });
+
+  work7.addEventListener("mouseover", function () {
+    document.getElementById("overtext7").style.display = "block";
+  });
+  work7.addEventListener("mouseout", function () {
+    document.getElementById("overtext7").style.display = "none";
+  });
+
+  work8.addEventListener("mouseover", function () {
+    document.getElementById("overtext8").style.display = "block";
+  });
+  work8.addEventListener("mouseout", function () {
+    document.getElementById("overtext8").style.display = "none";
+  });
+});
